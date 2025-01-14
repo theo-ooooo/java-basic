17dfe255947c8f35ff65201c2f28660d2ed3ae26
theo-ooooo <kkw.theo@gmail.com>
2025-01-14 08:30:55 +0000

    feature/section-4
    객제 지향 프로그래밍

---
+++ src/oop1/ValueObjectMain.java
@@ -0,0 +1,10 @@
+package oop1;
+
+public class ValueObjectMain {
+    public static void main(String[] args) {
+        ValueObject valueObject = new ValueObject();
+        valueObject.add();
+        valueObject.add();
+        valueObject.add();
+    }
+}

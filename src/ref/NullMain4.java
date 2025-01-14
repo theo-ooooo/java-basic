8c5c00b7bed13a50e1bd2c2f110b932b09204f25
theo-ooooo <kkw.theo@gmail.com>
2025-01-14 06:50:50 +0000

    feature/section-3
    NullPointerException

---
+++ src/ref/NullMain4.java
@@ -0,0 +1,12 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bigData = new BigData();
+        bigData.data = new Data();
+        System.out.println("BigData.count =" + bigData.count);
+        System.out.println("BigData.data =" + bigData.data);
+
+        System.out.println("BigData.data.value =" + bigData.data.value);
+    }
+}

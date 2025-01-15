7b9295e2f4297593864005be5e725f7c446daf39
theo-ooooo <kkw.theo@gmail.com>
2025-01-15 03:04:33 +0000

    add: 생성자 (문제와 풀이)

---
+++ src/construct/ex/Book.java
@@ -0,0 +1,24 @@
+package construct.ex;
+
+public class Book {
+    String title = "";
+    String author = "";
+    int page = 0;
+
+    public Book() {
+    }
+
+    public Book(String title, String author) {
+        this(title, author, 0);
+    }
+
+    public Book(String title, String author, int page) {
+        this.title = title;
+        this.author = author;
+        this.page = page;
+    }
+
+    public void displayInfo() {
+        System.out.println("제목: " + title + ", 저자: " + author + ", 페이지: " + page);
+    }
+}

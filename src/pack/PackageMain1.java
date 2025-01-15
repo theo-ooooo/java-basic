92b55cd815ba9d0f592dcac554ce6f87c7db1f53
theo-ooooo <kkw.theo@gmail.com>
2025-01-15 03:51:24 +0000

    add: 패키지 (패키지-import)

--- src/pack/PackageMain1.java
+++ src/pack/PackageMain1.java
@@ -1,9 +1,13 @@
 package pack;
 
 
+import pack.a.*;
+
 public class PackageMain1 {
     public static void main(String[] args) {
         Data data = new Data();
-        pack.a.User user = new pack.a.User();
+        User user = new User();
+
+        pack.b.User user1 = new pack.b.User();
     }
 }

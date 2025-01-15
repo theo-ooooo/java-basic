f5270fbd7f91e837ba1c5b1996f3596906c57ee7
theo-ooooo <kkw.theo@gmail.com>
2025-01-15 08:10:29 +0000

    add: 접근제어자 사용 - 클래스 레벨

---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,15 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultClass1 c1 = new DefaultClass1();
+        DefaultClass2 c2 = new DefaultClass2();
+    }
+}
+
+class DefaultClass1 {
+
+}
+
+class DefaultClass2 {}

bc85b1cf87c3850e03201de91e3393c7e1a00faf
Theo <kkw.theo@gmail.com>
2025-01-22 23:56:31 +0900

    add: 인터페이스

---
+++ src/poly/ex5/InterfaceMain.java
@@ -0,0 +1,21 @@
+package poly.ex5;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        // InterfaceAnimal interfaceAnimal = new InterfaceAnimal();
+
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Caw caw = new Caw();
+
+        soundAnimal(cat);
+        soundAnimal(dog);
+        soundAnimal(caw);
+    }
+
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}

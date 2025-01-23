0f35276f1e02bd68ac6e93b93f0df96dbf0381ca
kyungwon-kang <kkw.theo@gmail.com>
2025-01-23 02:04:49 +0000

    add: 클래스와 인터페이스 활용

---
+++ src/poly/ex6/BIrd.java
@@ -0,0 +1,13 @@
+package poly.ex6;
+
+public class BIrd extends AbstractAnimal implements Fly {
+    @Override
+    public void sound() {
+        System.out.println("짹짹");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("새 날기");
+    }
+}
